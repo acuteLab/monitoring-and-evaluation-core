ef40b90f17e020c910aca6362232418f172c5ecf
Ismaili Mohamedi <ismailimohamedi1995@gmail.com>
2022-06-30 16:53:28 +0300

    feat: add run.sh for starting project

---
+++ project_m_n_e_service/run.sh
@@ -0,0 +1,14 @@
+
+# Colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${green} ===== Development Server ===== ${reset}"
+
+# cd project_m_n_e_service
+echo "${green}>>> activate the Virtual Environment ${reset}"
+source $(pipenv --venv)/bin/activate
+sleep 2
+echo "${green}>>> Running server (Project_m_n_e_service Server) ${reset}"
+python manage.py runserver 
