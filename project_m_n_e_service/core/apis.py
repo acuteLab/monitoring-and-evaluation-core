3a7bad0797ee32d76941cd9721873c984f591894
Ismaili Mohamedi <ismailimohamedi1995@gmail.com>
2022-07-14 15:40:12 +0300

    Add get country ny countryCode

--- project_m_n_e_service/core/apis.py
+++ project_m_n_e_service/core/apis.py
@@ -6,3 +6,8 @@ country_api = Router()
 @country_api.get("/countries")
 def get_countries(request):
     return Country.get_countries(request)
+
+
+@country_api.get("/country")
+def get_country(request, countryCode: str):
+    return Country.get_country_by_code(request, countryCode)
--- project_m_n_e_service/core/views.py
+++ project_m_n_e_service/core/views.py
@@ -7,6 +7,12 @@ class Country:
       
     def get_countries(request):
         try:
-          return list(countries)
+          return [{'country_code': country_code, 'country_name': country_name} for country_code, country_name in  list(countries)]
         except:
-            return "Inernal Server Error"
+            raise "Inernal Server Error"
+        
+    def get_country_by_code(request, country_code):
+        try:
+            return {'country_code': country_code, 'country_name': dict(countries)[country_code]}
+        except:
+            raise "Inernal Server Error"
